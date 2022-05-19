195a31688666e62c27fe8c7a5e6c9352393a7a44
Auca Maillot <aucacoyan@gmail.com>
2022-05-18 21:22:05 -0300

    fixed python 3.6

--- firebird.py
+++ firebird.py
@@ -1,26 +1,27 @@
+code = """
 import fdb
 import os
 
-print(
-    "el path C:\Program Files (x86)\Firebird\Firebird_3_0\\fbclient.dll existe: ",
-    os.path.isfile("C:\Program Files (x86)\Firebird\Firebird_3_0\\fbclient.dll"),
-)
-# os.add_dll_directory('C:\\Program Files (x86)\\Firebird\\Firebird_3_0\\bin\\fbclient.dll')
-# os.add_dll_directory(os.getcwd())
-
 user = "SYSDBA"
 password = "masterkey"
-file = "C:\\firebirdDatabase\\TEST.FDB"
+file = r"C:/firebirdDatabase/TEST.FDB"
+ruta = os.path.join('C:', '\Program Files (x86)', 'Firebird', 'Firebird_3_0', 'fbclient.dll')
+print(ruta)
+print('el archivo ', os.path.isfile(ruta), 'existe')
 
+fdb.load_api(ruta)
 
-fdb.load_api('C:\Program Files (x86)\Firebird\Firebird_3_0\\fbclient.dll')
-# The server is named 'bison'; the database file is at '/temp/test.db'.
 connection = fdb.connect(
-    database="C:\\firebirdDatabase\TEST.FDB", user=user, password=password
+    database=file, user=user, password=password
 )
 
 cursor = connection.cursor()
 
-cursor.excecute("select * from languages order by year_released")
+cursor.execute("SELECT * FROM RDB$RELATIONS")
+#cursor.execute('select * from languages')
 
 print(cursor.fetchall())
+"""
+
+exec(code)
+
